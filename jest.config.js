c178493ae4846e3630137e8112228278051c2430
托尼 <yinchao.zhou@hipac.cn>
2020-01-09 15:59:06 +0800

    improvement: 单测配置优化

---
+++ jest.config.js
@@ -0,0 +1,36 @@
+/**
+ * Created by tony on 2020/1/9
+ */
+module.exports = {
+  setupFiles: [
+    "<rootDir>/test/setup.js"
+  ],
+  testMatch: [
+    "<rootDir>/test/**/*.{spec,test}.{js,jsx,ts,tsx}"
+  ],
+  collectCoverageFrom: [
+    "<rootDir>/src/Main/**/*.{js,jsx,ts,tsx}",
+    "!<rootDir>/src/Main/**/*.d.ts"
+  ],
+  testEnvironment: "jsdom",
+  testURL: "http://localhost",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+    "^(?!.*\\.(js|jsx|ts|tsx|json)$)": "<rootDir>/config/jest/fileTransform.js"
+  },
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "^.+\\.(css|less)$"
+  ],
+  moduleNameMapper: {
+    "^.+\\.(css|less)$": "identity-obj-proxy"
+  },
+  moduleFileExtensions: [
+    "js",
+    "json",
+    "jsx",
+    "ts",
+    "tsx",
+    "node"
+  ]
+}
